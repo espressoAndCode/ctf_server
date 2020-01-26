9f00f8e830414f875dca7727b21aa13367864db0
Wes Bailey <espressoandcode@gmail.com>
2020-01-26 13:21:09 -0500

    Add CORS module to Flask API

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from json import dumps
 
 from routes.scores import Scores
 
 app = Flask(__name__)
+CORS(app)
 app.config['DEBUG'] = True
 api = Api(app)
 
